98a68fde84392bb41dc21ec514b27534a7409baa
Juan Caballero <juandacf@gmail.com>
2025-05-13 16:23:08 -0500

    feat: :card_file_box: create singleton database connection script

---
+++ infrastructure/PostgreSQL/ConexionPostgresSingleton.cs
@@ -0,0 +1,35 @@
+using System;
+using Npgsql;
+
+namespace campuslove.infrastructure.postgreSQL
+{
+public class ConexionPostgresSingleton
+{
+private static ConexionPostgresSingleton _instancia;
+private readonly string _connectionString;
+private NpgsqlConnection _conexion;
+private ConexionPostgresSingleton(string connectionString)
+{
+_connectionString = connectionString;
+}
+public static ConexionPostgresSingleton Instancia(string connectionString)
+{
+_instancia ??= new ConexionPostgresSingleton(connectionString);
+return _instancia;
+}
+public NpgsqlConnection ObtenerConexion()
+{
+_conexion ??= new NpgsqlConnection(_connectionString);
+
+if (_conexion.State != System.Data.ConnectionState.Open)
+_conexion.Open();
+
+return _conexion;
+}
+
+        internal object ObetenerConexion()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ infrastructure/PostgreSQL/PostgresDbFactory.cs
@@ -0,0 +1,12 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace campuslove.infrastructure.PostgreSQL
+{
+    public class PostgresDbFactory
+    {
+        
+    }
+}

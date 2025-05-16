7a82bcafde45ccc67dc3e6927b3a78f4c09613cb
Juan Caballero <juandacf@gmail.com>
2025-05-16 16:17:59 -0500

    Menu admin_usuarios terminado

--- application/UI/UIAdminUsuarios.cs
+++ application/UI/UIAdminUsuarios.cs
@@ -1,4 +1,9 @@
+using System.Reflection;
+using campuslove.application.services;
+using campuslove.application.UI;
 using campuslove.domain.entities;
+using campuslove.domain.factory;
+using campuslove.infrastructure.PostgreSQL;
 using campusLove.application.UI;
 namespace CampusLove.application.UI
 {
@@ -6,6 +11,9 @@ namespace CampusLove.application.UI
     {
         public static void MenuAdminUsuarios()
         {
+            IDbFactory factory = new PostgresDbFactory(DbParameters.Parameters);
+            var ServicioCarrera = new CarreraService(factory.CreateCarreraRepository());
+            var ServicioUsuario = new UsuarioService(factory.CreateUsuarioRepository());
             while (true)
             {
                 Console.Clear();
@@ -25,33 +33,82 @@ namespace CampusLove.application.UI
                         Console.WriteLine("Por favor, ingrese la contraseña del usuario: ");
                         string ContraseñaUsuario = Console.ReadLine();
                         Console.WriteLine("Por favor, ingrese el género del usuario h/m");
-                        ConsoleKeyInfo keyInfo = Console.ReadKey();
-                         string teclaComoString = keyInfo.KeyChar.ToString();
-                        if (teclaComoString == 'h')
-                        {
-
-                        }
-
-                        
+                        Console.Clear();
+                        ServicioCarrera.VerCarrera();
+                        Console.WriteLine("Por favor, ingrese el id de la carrera del usuario: ");
+                        int IdCarreraUsuario = int.Parse(Console.ReadLine());
+                        bool GeneroUsuario = UIUtils.VerificadorGenero();
                         Usuario usuario = new Usuario
-                        { 
-                            
-                        };
+                        {
+                            cedula_ciudadania = CedulaCiudadania,
+                            nombre = NombreUsuario,
+                            apellido = ApellidoUsuario,
+                            contraseña = ContraseñaUsuario,
+                            genero = GeneroUsuario,
+                            id_carrera = IdCarreraUsuario
 
+                        };
+                        ServicioUsuario.CrearUsuario(usuario);
+                        Console.WriteLine("El usuario ha sido creado con éxito. Presione enter para volver al menú:");
+                        Console.ReadKey(true);
                         break;
                     case '2':
+                        Console.Clear();
+                        Console.WriteLine("Por favor, ingrese la cédula de ciudadanía del usuario a editar.");
+                        string CedulaCiudadaniaE = Console.ReadLine();
+                        Console.WriteLine("Por favor, ingrese el nuevo nombre del usuario:");
+                        string NombreUsuarioE = Console.ReadLine();
+                        Console.WriteLine("Por favor, ingrese el nuevo apellido del usuario: ");
+                        string ApellidoUsuarioE = Console.ReadLine();
+                        Console.WriteLine("Por favor, ingrese la nueva contraseña del usuario: ");
+                        string ContraseñaUsuarioE = Console.ReadLine();
+                        Console.WriteLine("Por favor, ingrese el nuevo género del usuario h/m");
+                        Console.Clear();
+                        ServicioCarrera.VerCarrera();
+                        Console.WriteLine("Por favor, ingrese el id de la nueva carrera del usuario: ");
+                        int IdCarreraUsuarioE = int.Parse(Console.ReadLine());
+                        bool GeneroUsuarioE = UIUtils.VerificadorGenero();
+                        Usuario usuarioE = new Usuario
+                        {
+                            cedula_ciudadania = CedulaCiudadaniaE,
+                            nombre = NombreUsuarioE,
+                            apellido = ApellidoUsuarioE,
+                            contraseña = ContraseñaUsuarioE,
+                            genero = GeneroUsuarioE,
+                            id_carrera = IdCarreraUsuarioE
+
+                        };
+                        ServicioUsuario.EditarUsuario(usuarioE);
+                        Console.WriteLine("El usuario ha sido creado con éxito. Presione enter para volver al menú:");
+                        Console.ReadKey(true);
                         break;
                     case '3':
+                        Console.Clear();
+                        ServicioUsuario.VerUsuarios();
+                        Console.WriteLine("Por favor, presione enter para volver al menú:");
+                        Console.ReadKey(true);
                         break;
                     case '4':
+                        Console.Clear();
+                        ServicioUsuario.VerUsuarios();
+                        Console.WriteLine("Por favor, digite la cédula del usuario a eliminar ");
+                        string CedulaUsuarioEliminar = Console.ReadLine();
+                        ServicioUsuario.EliminarUsuario(CedulaUsuarioEliminar);
+                        Console.WriteLine("¡El usuario ha sido eliminado con éxito! Por favor, presione enter para volver al menú.");
+                        Console.ReadKey(true);
+                        break;
+                    case '0':
+                        UIAdmin.MenuAdmin();
                         break;
                     default:
-                    Console.WriteLine("La opción ingresada no coincide con ninguna de nuestras opciones. Por favor, presione enter e inténtelo de nuevo.");
+                        Console.WriteLine("La opción ingresada no coincide con ninguna de nuestras opciones. Por favor, presione enter e inténtelo de nuevo.");
                         Console.ReadKey();
                         break;
                 }
             }
-            
+
         }
+
+        
     }
 }
---
+++ application/UI/UIUtils.cs
@@ -0,0 +1,29 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace campuslove.application.UI
+{
+    public class UIUtils
+    {
+        public static bool VerificadorGenero()
+        {
+            Console.Clear();
+            char keyInfo = Console.ReadKey(true).KeyChar;
+
+            switch (keyInfo)
+            {
+                case 'h':
+                    return true;
+                case 'm':
+                    return false;
+                default:
+                    Console.WriteLine("La opción ingresada no corresponde con ninguna de nuestras opciones de género. Presione enter, y vuelva a empezar.");
+                    Console.ReadKey();
+                    VerificadorGenero();
+                    return false;
+            }
+        }
+    }
+}
